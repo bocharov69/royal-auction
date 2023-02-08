36da459421ae2437fcf68c64901212bababcbef2
unknown <antonanon.mdr@gmail.com>
2023-02-08 19:52:20 +0200

    settings

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var style_root = document.querySelector(':root')
+    const style_root = document.querySelector(':root')
 
     //sidebar menu current page highlight
     const current_page = document.querySelector('.page').id;
@@ -31,29 +31,15 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     //header menu language icon change
-    var user_lang = navigator.language || navigator.userLanguage;
+    let user_lang = navigator.language || navigator.userLanguage;
     document.getElementById('header__menu-link--language').style = `background: url('./img/icons/languages/${user_lang}.svg') center / contain no-repeat;`
-
-    //user country select
-    var country_select = document.querySelector('.user-country-select');
-    var country_value = this.documentElement.querySelectorAll('.user-country-value');
-    country_select.style = `background: url("../../img/icons/flags/${country_select.value}.png") left center / 40px 20px no-repeat;`
-    country_value.forEach(element => {
-        element.textContent = document.querySelector(`.user-country-option[value="${country_select.value}"]`).textContent;
-    });
-    country_select.addEventListener("change" , function() {
-        country_select.style = `background: url("../../img/icons/flags/${country_select.value}.png") left center / 40px 20px no-repeat;`
-        country_value.forEach(element => {
-            element.textContent = document.querySelector(`.user-country-option[value="${country_select.value}"]`).textContent;
-        });
-    });
     
     //page tab change
-    var page_tab_btns = document.querySelectorAll('.page__tab-btn');
+    const page_tab_btns = document.querySelectorAll('.page__tab-btn');
 
     if (page_tab_btns.length > 0) {
         for (let i = 0; i < page_tab_btns.length; i++) {
-            var id = page_tab_btns[i].getAttribute("for");
+            let id = page_tab_btns[i].getAttribute("for");
 
             if (page_tab_btns[i].classList.contains('is-active')) {
                 document.getElementById(id).classList.add('is-active');
@@ -61,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.getElementById(id).classList.remove('is-active');
 
             page_tab_btns[i].addEventListener("click", function (e) {
-                var other_tab;
+                let other_tab;
                 id = e.target.getAttribute("for");
                 if (page_tab_btns[i - 1])
                     other_tab = page_tab_btns[i - 1];
@@ -79,10 +65,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     //barchart !!(required defined width and height on main parent (parent of barchart-wrapper))
-    var barcharts = document.querySelectorAll('.barchart');
+    const barcharts = document.querySelectorAll('.barchart');
     if (barcharts.length > 0) {
         barcharts.forEach(barchart => {
-            var data = [
+            let data = [
                 [1, 12000],
                 [2, 15000],
                 [3, 31000],
@@ -92,17 +78,17 @@ document.addEventListener('DOMContentLoaded', function () {
                 [7, 20000],
             ]; // [index, key]
 
-            var period = 'week'; //week, month
-            var monthTicks = ["Янв", "Февр", "Март", "Апр", "Май", "Июнь", "Июль", "Авг", "Сент", "Окт", "Нояб", "Дек"];
-            var weekTicks = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
-            var currency = 'roto';
-            var currencyText = 'ROTO';
-            var currencyColor = '#2F53FF';
+            let period = 'week'; //week, month
+            let monthTicks = ["Янв", "Февр", "Март", "Апр", "Май", "Июнь", "Июль", "Авг", "Сент", "Окт", "Нояб", "Дек"];
+            let weekTicks = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
+            let currency = 'roto';
+            let currencyText = 'ROTO';
+            let currencyColor = '#2F53FF';
 
-            var barchartLegend = barchart.parentElement.querySelector(".barchart-heading-legend");
-            var barchartTotal = barchart.parentElement.querySelector(".barchart-heading-total-span");
-            var nav_select = barchart.parentElement.querySelector('.barchart-nav-select');
-            var nav_radio_labels = barchart.parentElement.querySelectorAll('.barchart-nav-radio-label');
+            let barchartLegend = barchart.parentElement.querySelector(".barchart-heading-legend");
+            let barchartTotal = barchart.parentElement.querySelector(".barchart-heading-total-span");
+            let nav_select = barchart.parentElement.querySelector('.barchart-nav-select');
+            let nav_radio_labels = barchart.parentElement.querySelectorAll('.barchart-nav-radio-label');
 
             nav_radio_labels.forEach(element => {
                 if (element.previousElementSibling.checked == true) {
@@ -165,9 +151,9 @@ document.addEventListener('DOMContentLoaded', function () {
             function drawBarChart() {
                 barchart.textContent = ''; //delete old barchart
 
-                var totalProfit = 0;
+                let totalProfit = 0;
 
-                var xAxis = document.createElement('div');
+                let xAxis = document.createElement('div');
                 xAxis.classList.add('.barchart--xAxis');
                 xAxis.style.position = 'absolute';
                 xAxis.style.left = '0';
@@ -177,7 +163,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 xAxis.style.backgroundColor = 'rgba(255,255,255,0.1)';
                 barchart.appendChild(xAxis);
 
-                var yAxis = document.createElement('div');
+                let yAxis = document.createElement('div');
                 yAxis.classList.add('.barchart--yAxis');
                 yAxis.style.position = 'absolute';
                 yAxis.style.top = '-10px';
@@ -188,10 +174,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 yAxis.style.backgroundColor = 'rgba(255,255,255,0.1)';
                 barchart.appendChild(yAxis);
 
-                var xMax = 0;
-                var yMax = 0;
+                let xMax = 0;
+                let yMax = 0;
 
-                for (var i = 0; i < data.length; i++) {
+                for (let i = 0; i < data.length; i++) {
                     if (data[i][0] > xMax) {
                         xMax = data[i][0];
                     }
@@ -200,11 +186,11 @@ document.addEventListener('DOMContentLoaded', function () {
                     }
                 }
 
-                var xScale = xAxis.offsetWidth / (xMax + 1);
-                var yScale = (yAxis.offsetHeight - 10) / yMax;
+                let xScale = xAxis.offsetWidth / (xMax + 1);
+                let yScale = (yAxis.offsetHeight - 10) / yMax;
 
-                for (var i = 0; i < data.length; i++) {
-                    var bar = document.createElement('div');
+                for (let i = 0; i < data.length; i++) {
+                    let bar = document.createElement('div');
                     bar.classList.add('barchart-bar');
                     bar.style.height = (data[i][1] * yScale) + 'px';
                     bar.style.backgroundColor = currencyColor;
@@ -217,7 +203,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     bar.style.width = '2.5%';
                     bar.style.borderRadius = '24px';
 
-                    var barLabel = document.createElement('div');
+                    let barLabel = document.createElement('div');
                     barLabel.classList.add('barchart-bar-label');
                     barLabel.textContent = data[i][1];
                     barLabel.style.color = currencyColor;
@@ -234,8 +220,8 @@ document.addEventListener('DOMContentLoaded', function () {
                     bar.appendChild(barLabel);
 
 
-                    var xTick = document.createElement('div');
-                    var xTick_line = document.createElement('div');
+                    let xTick = document.createElement('div');
+                    let xTick_line = document.createElement('div');
                     xTick.classList.add('barchart-xAxis-tick');
                     if (period === 'week')
                         xTick.textContent = weekTicks[data[i][0] - 1];
@@ -259,8 +245,8 @@ document.addEventListener('DOMContentLoaded', function () {
                     barchart.appendChild(xTick);
 
 
-                    var yTick = document.createElement('div');
-                    var yTick_line = document.createElement('div');
+                    let yTick = document.createElement('div');
+                    let yTick_line = document.createElement('div');
                     yTick.classList.add('barchart-yAxis-tick');
                     yTick.textContent = data[i][1];
                     yTick.style.position = 'absolute';
